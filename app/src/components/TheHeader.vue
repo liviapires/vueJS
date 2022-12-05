118d9c1eedfcd7edf68e40d0c289d595371230ab
liviapires <livia.onofre@fatec.sp.gov.br>
2022-12-05 12:01:13 -0300

    Criando projeto vue cru

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <h1>Header</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+    .header {
+        background-color: #000;
+        color: #fff;
+    }
+</style>
