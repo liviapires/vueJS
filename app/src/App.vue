7f8864d1af857e2180de4a20ed9db73814a08c6e
liviapires <livia.onofre@fatec.sp.gov.br>
2022-12-06 15:57:29 -0300

    Continuação do curso

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,39 +1,37 @@
 <template>
-  <TheHeader 
-    v-show="showHeader"
-  />
-  <img 
-    alt="Vue logo" 
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ExemplosDiretivas v-if="showHeader"/>
+  
+  <br>
+  
+  <div v-if="accessLevel === 'admin'">Usuário Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
+  <div v-else>Usuário</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'
+import ExemplosDiretivas from './components/ExemplosDiretivas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    TheHeader
+    ExemplosDiretivas,
   },
   data() {
     return {
-      showHeader: false
+      showHeader: true,
+      accessLevel: 'admin',
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ app/src/components/ExemplosDiretivas.vue
@@ -0,0 +1,217 @@
+<template>
+    <div v-show="showName">{{ fullName }}</div>
+
+    <div>
+        <h1>Ligação de Dados</h1>
+
+        <h2 :class="classVar">XXX</h2>
+        <h2 :class="{'title': false, 'title-home': isHome}">YYY</h2>
+        
+        <p :class="pClass">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem at eos qui deleniti minus numquam repellendus! Culpa minus ducimus numquam, repellat sit eligendi eius harum accusamus suscipit illo sunt dignissimos!
+        </p>
+    </div>
+
+    <div>
+        <div>
+            <h1>Ligação de Dados Bidirecional</h1>
+            <label>Nome: </label>
+            <input v-model="name" type="text"/>
+
+            <br>
+            <br>
+
+            {{ name }}
+        </div>
+
+        <div>
+            <label>Esporte: </label>
+            <select v-model="sports">
+                <option value="">Escolha</option>
+                <option value="Futebol">Futebol</option>
+                <option value="Basquete">Basquete</option>
+                <option value="Vôlei">Vôlei</option>
+            </select>
+
+            <br><br>
+
+            {{ sports }}
+        </div>
+
+        <div>
+            <label>Opção: </label>
+            <br>
+            <input v-model="opcao" type="radio" value="Sim"> Sim
+            <input v-model="opcao" type="radio" value="Não"> Não
+
+            <br>
+            <br>
+
+            {{ opcao }}
+        </div>
+
+        <div>
+            <label>Checkbox: </label>
+            <br>
+            <input v-model="checkbox" type="checkbox"> Aceitar
+
+            <br>
+            <br>
+
+            {{ checkbox }}
+        </div>
+
+        <div>
+            <label>Cores Favoritas: </label>
+            <br>
+            <input v-model="colors" type="checkbox" value="Azul"> Azul
+            <br>
+            <input v-model="colors" type="checkbox" value="Amarelo"> Amarelo
+            <br>
+            <input v-model="colors" type="checkbox" value="Verde"> Verde
+            <br>
+            <input v-model="colors" type="checkbox" value="Vermelho"> Vermelho
+
+            <br>
+            <br>
+
+            {{ colors }}
+        </div>
+    </div>
+
+    <div>
+        <button v-on:click="onClick">
+            Click
+        </button>
+    </div>
+
+    <div @mouseover="onMouseOver">
+            Olá
+    </div>
+
+    <div>
+        <div v-for="(obj, index) in todos" :key="obj.id" class="todos-items">
+            <img v-if="obj.img" :src="obj.img"/>
+            {{ index }} - {{ obj.title }}
+        </div>
+    </div>
+
+    <div>
+        <form action="https://google.com" @submit="onSubmit">
+            <button type="submit">
+                Enviar
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ExemplosDiretivas',
+        data() {
+            return {
+                user: {
+                    firstName: 'Livia',
+                    lastName: 'Pires',
+                },
+                showName: true,
+                classVar: 'title',
+                isHome: true,
+                pClass: ['text', 'text-home'],
+                name: "Oi",
+                sports: "",
+                opcao: "",
+                checkbox: true,
+                colors: [],
+                todos: [
+                    {
+                        "userId": 1,
+                        "id": 1,
+                        "title": "delectus aut autem",
+                        "completed": false,
+                        "img": "https://via.placeholder.com/20"
+                    },
+                    {
+                        "userId": 1,
+                        "id": 2,
+                        "title": "quis ut nam facilis et officia qui",
+                        "completed": false
+                    },
+                    {
+                        "userId": 1,
+                        "id": 3,
+                        "title": "fugiat veniam minus",
+                        "completed": false
+                    },
+                    {
+                        "userId": 1,
+                        "id": 4,
+                        "title": "et porro tempora",
+                        "completed": true,
+                        "img": "https://via.placeholder.com/20"
+                    },
+                    {
+                        "userId": 1,
+                        "id": 5,
+                        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                        "completed": false
+                    }
+                ]
+
+            }
+        },
+
+        methods: {
+            onClick() {
+                console.log("Click");
+            },
+            onMouseOver(){
+                console.log("Olá");
+            },
+            onSubmit(){
+                console.log("Submit");
+            },
+        },
+
+        computed: {
+            fullName(){
+                return `${this.user.firstName} ${this.user.lastName}`
+            },
+        },
+    }
+</script>
+
+<style>
+    .title {
+        color: greenyellow;
+        padding: 0px;
+    }
+
+    .title-home{
+        color: green;
+        padding: 0px;
+    }
+
+    .text{
+        color: #fefefe;
+        background-color: gray;
+        padding: 10px
+    }
+
+    .text-home{
+        color: #000;
+        background-color: yellow
+    }
+
+    .todos-items {
+        background-color: #aaa;
+        color: #fefefe;
+        padding: 5px;
+        margin-top: 6px;
+        text-align: justify;
+    }
+
+    div{
+        margin-top: 50px;
+    }
+</style>
