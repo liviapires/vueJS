9e79134aa086d31940547557682a270078fa2828
liviapires <livia.onofre@fatec.sp.gov.br>
2022-12-08 14:37:11 -0300

    Adicionando rotas

---
+++ Vue Route/novo_vue/src/views/UserEditView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Usuário - nº {{ $route.params.id }}
+    </div>
+    <br>
+    <button @click="login()">
+        Clique Aqui
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'UserEditView',
+        methods:{
+            login(){
+                this.$router.push('/about')
+            }
+        }
+    }
+</script>
