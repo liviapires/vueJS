c62b56b103a861a1a2a64ac59ac5c9ef248a2382
liviapires <livia.onofre@fatec.sp.gov.br>
2022-12-13 11:00:47 -0300

    Separando componentes

---
+++ todolistvue/src/components/ToDoItems.vue
@@ -0,0 +1,18 @@
+<template>
+    
+
+    <div class="space-y-2">
+        <ToDoItem v-for="todo in $store.state.todos" :key="todo.id" :todo="todo"/>
+    </div>
+</template>
+
+<script>
+import ToDoItem from './ToDoItem.vue'
+
+export default {
+    name: 'ToDoItems',
+    components:{
+        ToDoItem,
+    }
+}
+</script>
